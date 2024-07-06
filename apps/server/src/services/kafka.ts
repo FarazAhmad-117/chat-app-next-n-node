c65ab83d0f8a5599e73509bb3384548a9ac1f62f
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-06 22:11:21 +0500

    I am going to leave it kafka is too expensive

---
+++ apps/server/src/services/kafka.ts
@@ -0,0 +1,15 @@
+import {Kafka} from 'kafkajs'
+
+const kafka  = new Kafka({
+    brokers:['kafka-37464a69-scalable-chat-app-next-node.h.aivencloud.com:10739'],
+    sasl:{
+        username:'',
+        password:''
+    }
+})
+
+
+
+
+
+
